c324caa91d1a42350459ddc76d0df83e8741a8bb
Myos <myos.chen@gmail.com>
2023-12-16 14:30:23 +0800

    chore: use stylistic

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,57 @@
+const stylistic = require('@stylistic/eslint-plugin');
+
+const customized = stylistic.configs.customize({
+  flat: false,
+  indent: 2,
+  jsx: true,
+  quotes: 'single',
+  semi: false,
+});
+
+/** @type {import('eslint').Linter.Config'} */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier'],
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@stylistic', 'simple-import-sort', 'unused-imports'],
   rules: {
-    'prettier/prettier': ['error'],
+    'react-refresh/only-export-components': [
+      'warn',
+      {allowConstantExport: true},
+    ],
+
+    ...customized.rules,
+    '@stylistic/jsx-curly-brace-presence': ['error', 'always'],'simple-import-sort/exports': 'error',
+
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^\\u0000'],
+          ['^react', '^next', '@?\\w'],
+          ['^~/.*'],
+          ['^@/.*'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
+    ],
+
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+  },
+  settings: {
+    tailwindcss: {
+      callees: ['cn'],
+    },
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'node:path';
+import paths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-    },
-  },
+  plugins: [react(), paths()],
 });
