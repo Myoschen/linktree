ebb56847a93da6acbd2aef137fb5a2480148b445
Myos <myos.chen@gmail.com>
2023-12-16 18:11:44 +0800

    fix: add theme switch

--- src/pages/linktree.tsx
+++ src/pages/linktree.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useSearchParams } from 'react-router-dom'
 import { decode } from 'js-base64'
 
 import Linktree from '@/components/linktree'
+import ThemeSwitch from '@/components/theme-switch'
 
 export default function LinktreePage() {
   const [searchParams] = useSearchParams()
@@ -15,5 +16,10 @@ export default function LinktreePage() {
     }
   }, [data, navigate])
 
-  return <Linktree data={JSON.parse(decode(data!))} />
+  return (
+    <>
+      <Linktree data={JSON.parse(decode(data!))} />
+      <ThemeSwitch className={'fixed right-4 top-4'} />
+    </>
+  )
 }
